0cb5af85e33e58cce2e0d1d3685fea78aefee4ff
sow4063 <sow4063@gmail.com>
2017-05-27 17:47:00 +0900

    Moved fortune cookes into module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
@@ -9,21 +10,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-  'Conquer your fears or they will conquer you.',
-  'Rivers need springs',
-  "Do not fear what you don't know.",
-  'You will have a pleasant surprise.',
-  'Whenever possible, keep it simple.',
-];
-
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', {fortune: randomFortune});
+	res.render('about', { fortune: fortune.getFortune() } );
 });
 
 app.use(function(req, res, next){
